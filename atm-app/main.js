d55c2aede2dfefc917885ed3052b5ee0d2dfee24
Bragodin <40167436+Bragodin@users.noreply.github.com>
2020-01-04 15:56:28 +0300

    Update main.js

--- atm-app/main.js
+++ atm-app/main.js
@@ -1,123 +1,165 @@
 class EventEmitter {
-    constructor() {
-      this.events = {};
-    }
-    
-    emit(eventName, data) {
-      const event = this.events[eventName];
-      if( event ) {
-        event.forEach(fn => {
-          fn.call(null, data);
-        });
-      }
-    }
-    
-    subscribe(eventName, fn) {
-      if(!this.events[eventName]) {
-        this.events[eventName] = [];
-      }
-      
-      this.events[eventName].push(fn);
-      return () => {
-        this.events[eventName] = this.events[eventName].filter(eventFn => fn !== eventFn);
-      }
-    }
-
+  constructor() {
+    this._events = {};
   }
+  on(evt, listener) {
+    (this._events[evt] || (this._events[evt] = [])).push(listener);
+    return this;
+  }
+  emit(evt, arg) {
+    (this._events[evt] || []).slice().forEach(lsn => lsn(arg));
+  }
+}
 
-class Atm {
-    constructor(id){
-        this.time = Math.random() * 3000;
-        this.state = true;
-        this.atmId = id;
-        this.personInAtm = null;
-    }
+class Atm extends EventEmitter {
+  constructor(id){
+    super();
+    this.time = Math.random() * (5000 - 2000) + 2000;
+    this.state = true;
+    this.atmId = id;
+    this.personInAtm = null;
+    this.checkState();
+  }
+  changeState(){
+    this.state = false;
+    setTimeout(() => {
+      this.state = true;
+    }, this.time);
+  }
+  checkState() {
+    setTimeout(()=> {
+      if(this.state === true){
+        this.emit('freeAtm', this.atmId);
+      }
+      return this.checkState();
+    }, 0);
+  }
 }
 
 class Person {
-    constructor(){
-      this.randoTimeInAtm =  Math.random() * 5000;
-    }
+  constructor(id){
+    this.personId = id;
+  }
 };
 //model
-class Model {
-    constructor(){
-        this.turn = [];
-        this.atmsArray = [];
-        //
-        this.eventOfAddPerson = new Event('addPerson');
-
-        let i = 0;
-        while(i < 3){
-            this.atmsArray.push(new Atm(i));
-            i++;
+class Model extends EventEmitter {
+  constructor(){
+      super();
+      this.turn = [];
+      this.atmsArray = [];
+      this.addPersonToTurn();
+  }
+  addAtms(i){
+    this.atmsArray.push(new Atm(i));
+  }
+  addPersonToTurn(){   
+    setTimeout(() => {
+      if(this.turn.length < 15){
+        if(this.turn.length !== 0){
+          let personId = (this.turn[this.turn.length - 1].personId + 1);
+          this.turn.push(new Person(personId));
+          this.emit('personAdded');
+        } else {
+          let personId = 1;
+          this.turn.push(new Person(personId));
+          this.emit('personAdded');
         }
-    }
-    getTurn(){
-        return this.turn;
-    }
-    addPersonToTurn(){
-        this.turn.push(new Person());
-        console.log(this.turn);
-    }
-    removePersonToTurn(){
-        this.turn.shift(turn[0]);
-    }
+      } 
+      return this.addPersonToTurn();
+    }, 500);
+  }
+  removePersonFromTurn(){
+      this.turn.shift(this.turn[0]);
+  }
 }
 
-class View {
-    constructor(){
-        this.divForAtm = document.createElement('div');
-        this.divForAtm.className = "div-for-atm";
-        
-        this.ul = document.createElement('ul');
-        this.ul.className = "ul__turn";
+class View extends EventEmitter {
+  constructor(){
+      super();
+      this.divForAtm = document.createElement('div');
+      this.divForAtm.className = "div-for-atm";
+      
+      this.ul = document.createElement('ul');
+      this.ul.className = "ul__turn";
 
-        document.body.append(this.divForAtm);
-        document.body.append(this.ul);
+      document.body.append(this.divForAtm);
+      document.body.append(this.ul);
+  }
+  addPersonToTurn(number){
+      let li = document.createElement('li');
+      li.className = "ul__li_person";
+      li.innerHTML = `<h3>${number}</h3>`;
+      this.ul.append(li);
+  }
+  createAtm(){
+    let atm = document.createElement('div');
+    atm.className = "atm";
+    this.divForAtm.append(atm);
+  }
+  removePersonFromTurn(){
+      let firstElementOfTurn = document.getElementsByClassName('ul__li_person');
+      console.log(firstElementOfTurn);
 
-    }
-    addPersonToTurn(){
-        let li = document.createElement('li');
-        li.className = "ul__li_person";
-        this.ul.append(li);
-    }
-    removePersonToTurn(){
-        
-        
-    }
+      let li = this.ul.firstElementChild;
+      this.ul.removeChild(li);
+  }
+  addPersonToAtm(atmNumber, personId, time){
+    let atms = document.getElementsByClassName('atm');
+    let li = document.createElement('div');
+    li.className = "ul__li_person";
+    li.innerHTML = `<h3>${personId}</h3>`;
+    atms[atmNumber].append(li);
+    setTimeout(() => {
+      atms[atmNumber].removeChild(li);////  CHANGE TIME
+    }, time);
+  }
 }
-
 //controller
-class Controller {
-    constructor(model, view){
-        this.model = model;
-        this.view = view;
-        this.view.addPersonToTurn();
-        this.createPerson();
-        this.createAtm();
-     
+class Controller extends EventEmitter {
+  constructor(model, view){
+      super();
+      this.model = model;
+      this.view = view;
+      this.createAtm();
+      this.createPerson();
+      this.checkAtm();
+      this.checkAtm();
+  }
+  createAtm(){
+    let j = 0;
+    while(j < 3){
+      this.view.createAtm();
+      this.model.addAtms(j);
+      j++;
+    }
+  }
+  createPerson(){
+    this.model.on('personAdded', () => { this.view.addPersonToTurn(this.model.turn[this.model.turn.length - 1].personId) });
+  }
+  checkAtm(){
+    setInterval(() => {
+      let freeAtmIndex;
+      let freeAtm = this.model.atmsArray.find((el) => {
+        freeAtmIndex = el.atmId;
+        return el.state === true;
+      });
+      if(freeAtm && this.model.turn.length > 0){
+        this.model.atmsArray[freeAtmIndex].changeState();
+        this.view.removePersonFromTurn();
 
-           
-    }
-    createAtm(){
-       setInterval(() => {
-        console.log(this.model.atmsArray); 
-       }, 3000);
-    }
-    createPerson(){
-        setTimeout(() => {
-            this.model.addPersonToTurn();
-            this.view.addPersonToTurn();
-            return this.createPerson();
-        }, Math.random() * 3000);
-    }
-    updateTurn(){
-        // setInterval(() => {
-        //    this.view.addPersonToTurn();
-        // }, 3000);
-    }
+        this.view.addPersonToAtm(freeAtmIndex, this.model.turn[0].personId, freeAtm.time);
+        this.model.removePersonFromTurn();
+      }
+    }, 0);
+
+  }
 }
 
+function stop(){
+  debugger;
+}
 
-const app = new Controller(new Model(), new View());
+function start(){
+  new Controller(new Model(), new View());
+}
+
