4ce7a0c1e56efe16170c36c46e5e363209ed4ba6
Bragodin <drozdegor.sr@gmail.com>
2020-01-28 20:21:21 +0300

    add cors url

--- server-express/src/index.js
+++ server-express/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const router = require('./routers/export-router');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 require('dotenv').config({ path: 'config/build.env'});
 
 mongoose.connect(process.env.MONGO_DB, {
@@ -13,6 +15,7 @@ mongoose.connect(process.env.MONGO_DB, {
 
 const port = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use('/', router.userRouter);
